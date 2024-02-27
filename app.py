6bff8c7e66a77f22e2fde82133431d8f5f435746
mrralr <161452797+mrralr@users.noreply.github.com>
2024-02-27 10:17:42 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,18 @@ import streamlit as st
 st.title("oboboooaoo")
 
 st.header("eesaesesa")
+
+st.subheader("shush")
+
+st.info("yuh")
+
+st.warning("acousitc")
+
+st.error("oopsie daisy")
+
+st.success("Well done")
+
+st.write("abdurahamna hasna")
+st.write(range(50))
+
+st.markdown("# hm")
