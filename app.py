96ed7ebdb75edb125f9ef7f60890d20151fa6e1e
mrralr <161452797+mrralr@users.noreply.github.com>
2024-03-05 11:40:55 +0000

    Update app.py

--- app.py
+++ app.py
@@ -26,9 +26,10 @@ global home_btn
 
 if st.session_state.home_btn:
   click_homebtn()
-  if st.session_state.home_btn:
+  
+if st.session_state.home_btn:
     home_btn = st.sidebar.button("Home", type="primary", use_container_width=True)
-  else:
+else:
     home_btn = st.sidebar.button("Home", type="secondary", use_container_width=True)
 
 if home_btn:
@@ -40,10 +41,10 @@ global la_btn
 
 if st.session_state.la_btn:
   click_labtn()
-  if st.session_state.la_btn:
-    la_btn = st.sidebar.button("Location Analysis", type="primary", use_container_width=True)
-  else:
-    la_btn = st.sidebar.button("Location Analysis", type="secondary", use_container_width=True)
+if st.session_state.la_btn:
+  la_btn = st.sidebar.button("Location Analysis", type="primary", use_container_width=True)
+else:
+  la_btn = st.sidebar.button("Location Analysis", type="secondary", use_container_width=True)
 
 if la_btn:
   click_labtn()
